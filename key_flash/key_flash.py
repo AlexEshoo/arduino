c288e78a6b105c2f803e64053fd80f7b1279a4bf
Alex Eshoo <AlexEshoo@gmail.com>
2016-10-02 22:09:59 -0400

    Reorg

--- key_flash/key_flash.py
+++ key_flash/key_flash.py
@@ -6,11 +6,12 @@ arduino = serial.Serial('COM3' , 115200, timeout=.1)
 time.sleep(1)
 
 while True:
-
-	if msvcrt.kbhit():
+	while msvcrt.kbhit():
 		x = msvcrt.getch()
 		arduino.write(x)
 		data = arduino.readline()
-	
+		
 		if data:
 			print data
+	
+	arduino.write(0)
