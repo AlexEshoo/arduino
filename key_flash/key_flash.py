0ae65b6b77ebcd4908d2c3db34b398d984749962
Alex Eshoo <AlexEshoo@gmail.com>
2016-10-13 22:56:14 -0400

    Comments

--- key_flash/key_flash.py
+++ key_flash/key_flash.py
@@ -7,6 +7,18 @@ time.sleep(1)
 flag = False
 
 def sendColor(R,G,B, led_ids = None):
+	"""
+	Sends a message over serial to the connected Arduino with RGB 
+	information and addresses of LEDs (optionally).
+	
+	Parameters:
+	R: Red value (from 0 to 127)
+	G: Green Value (from 0 to 127)
+	B: Blue value (from 0 to 127)
+	led_ids: A list of LEDs to be addressed.
+	
+	Returns: None.
+	"""
 	l = [R,G,B]
 	for c in l:
 		if c > 127 or c < 0:
@@ -29,10 +41,27 @@ def sendColor(R,G,B, led_ids = None):
 	
 	msg += "?"
 	arduino.write(msg)
+	
+# ################################################################################# #
+# Define the Multikey Binds. These are binds that involve multiple simultaneous 
+# Keypresses. Bind Objects are created and mapped to callback handlers. All objects 
+# are stored in the binds list on __init__. 
+# ################################################################################# #
 
 binds = []
 	
 class Bind(object):
+	"""
+	Container class for keybinds.
+	
+	Parameters:
+	trig: The trigger key to activate the keybind. This is the last key pressed 
+		  in the sequence.
+	mods: List of modifier keys that need to be held down when trigger key is 
+		  pressed.
+	handle: Callback function for the keybind. Executes if the keybind is 
+			recognized.
+	"""
 	def __init__(self,trig,mods,handle):
 		self.trigger_key = trig
 		self.mod_keys = mods
@@ -41,6 +70,18 @@ class Bind(object):
 		self.handler = handle
 		binds.append(self)
 
+def getBind(event):
+	"""
+	Get the bind on a keypress if there is one.
+	"""
+	for bind in binds:
+		if event.current_key == bind.trigger_key and event.pressed_key == bind.mod_keys:
+			return bind
+
+	return None
+
+# Keybind Callback Functions #
+	
 def ctrlJ_func(key, key_up=False):
 	print "crtlJ"
 	if key_up:
@@ -49,12 +90,13 @@ def ctrlJ_func(key, key_up=False):
 
 save = Bind('J',['Lcontrol'],ctrlJ_func)
 
-def goBind(event):
-	for bind in binds:
-		if event.current_key == bind.trigger_key and event.pressed_key == bind.mod_keys:
-			return bind
-
-	return None
+# ############################################################################################# #
+# Callback functions for individual keycode presses follow. These are stored in a dispatcher    #
+# dictionary named `dispatch`. Note: It would be possible to define single keypresses as a      #
+# Bind object with empty mod_keys list, however speed is key, so we want to keep the number of  #
+# objects that getBind() iterates over to a minimum. Therfore a dispatcher is ideal since it    #
+# will know the callback to use for a given key without need to check all of them.              #
+# ############################################################################################# #
 	
 def numberPress(num, key_up=False):
 	if int(num) in range(0,10):
@@ -75,6 +117,12 @@ def otherPress(key, key_up=False):
 	
 	sendColor(0,0,127)
 
+def enterPress(key, key_up=False):
+	if key_up:
+		return
+	
+	sendColor(0,127,0)
+	
 dispatch = {}
 for key in ID_TO_KEY:
 	item = ID_TO_KEY[key]
@@ -82,13 +130,22 @@ for key in ID_TO_KEY:
 		dispatch[item] = letterPress
 	elif item in [str(s)for s in range(0,10)] or item in ['Numpad{}'.format(s) for s in range(0,10)]:
 		dispatch[item] = numberPress
+	elif item == 'Enter':
+		dispatch[item] = enterPress
 	else:
 		dispatch[item] = otherPress
-		
+
 def handle_events(args):
+	"""
+	Main Handler Function. First checks for keybinds, then failing results in dispatching 
+	the handler for a 'key down' event, followed by a 'key up', and finally simply sending 
+	a message to turn off the LEDs.
+	
+	Parameters: PyHooked::KeyboardEvent object
+	"""
 	if isinstance(args, KeyboardEvent):
 		try:
-			goBind(args).handler(args.current_key)
+			getBind(args).handler(args.current_key)
 		except AttributeError:
 			if args.event_type == 'key down':
 				dispatch[args.current_key](args.current_key)
@@ -98,5 +155,5 @@ def handle_events(args):
 				sendColor(0,0,0)
 			
 hk = Hook()  # make a new instance of PyHooked
-hk.handler = handle_events  # add a new shortcut ctrl+a, or triggered on mouseover of (300,400)
+hk.handler = handle_events
 hk.hook()  # hook into the events, and listen to the presses
