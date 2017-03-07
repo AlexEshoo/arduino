7ac031c913d2ac5b9c0a0cc09e2c1a20b524d10a
Alex Eshoo <AlexEshoo@gmail.com>
2017-03-06 21:22:17 -0500

    Idle Mode
    Adds an idle mode based on output of 555 timer with about 27 second one
    shot monostable multivibrator mode. Requires wiring changes and flash
    change.

--- key_flash/key_flash_ino/key_flash_ino.ino
+++ key_flash/key_flash_ino/key_flash_ino.ino
@@ -1,38 +1,77 @@
 #include "LPD8806.h"
 #include "SPI.h"
 
-const int button_pin = 6;
+const int TRIGGER_PIN = 6;
+const int TIMER_OUT_PIN = 7;
+int timerState = 0;
+
 int nLEDs = 32;
 int dataPin = 2;
 int clockPin = 3;
-int potPin = A0;
+
+int i;
 
 LPD8806 strip = LPD8806(nLEDs,dataPin,clockPin);
 
-struct led {
-  int R;
-  int G;
-  int B;
-};
-
 void setup() {
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(TIMER_OUT_PIN, INPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  
   strip.begin();
   strip.show();
+  
   Serial.begin(115200);
 }
 
 String msg;
 
 void loop() {
-  if(Serial.available() > 0){
-    char data = Serial.read();
-    msg += data;
-
-    if (data == '?'){
-      parseMsg(msg);
-      msg = "";
-    }
+  timerState = digitalRead(TIMER_OUT_PIN);
+  if (timerState == LOW) {
+    goIdle();
   }
+  
+  else if(Serial.available() > 0){
+    Serial.print("Message Incoming");
+    triggerTimer();
+    while (Serial.available() > 0) {
+      Serial.read();
+    }
+    Serial.print("finished reading");
+    //char data = Serial.read();
+    //msg += data;
+
+    //if (data == '?'){
+      //parseMsg(msg);
+      //msg = "";
+    //}
+  }
+  else {
+    Serial.println(i);
+    delay(1000);
+    i++;
+  }
+} 
+
+void triggerTimer() {
+  digitalWrite(TRIGGER_PIN, LOW);
+  delay(100);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delay(10); //Allow time for 555 out to go HIGH again
+  return;
+}
+
+void goIdle() {
+  i=0;
+  while (Serial.available() == 0) {
+    digitalWrite(13, HIGH);
+    delay(100);
+  }
+  digitalWrite(13, LOW);
+  triggerTimer();
+  return;
 }
 
 void parseMsg(String data_frame){
